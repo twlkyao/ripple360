58ec087d434961b7e766871eb003718e84b50fb8
nyusternie <nyusternie@yahoo.com>
2013-03-31 16:36:03 -0400

    some re-formatting on the server, plus updates to front-end

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 Ripple 360
 ==========
 
+Ripple Game Engine / Server
+
 # Introduction
 
-Suite of Ripple games
+Simple front and back-end server for game development.
+
+# Front-end
+
+Available games
+ - Hi-Lo
--- server.js
+++ server.js
@@ -10,6 +10,7 @@
  */
 
 var WebSocket 	= require('ws');
+var fs 			= require('fs');
 var crypto 		= require('crypto');
 var winston 	= require('winston');
 var colors		= require('colors');
@@ -86,46 +87,87 @@ var logger = new (winston.Logger)(
 // add logger colors
 winston.addColors(loggerConfig.colors);
 
-// connect to RIPPLE socket server
-var ws = new WebSocket(RIPPLE_SERVER);
+// initialize web socket instance
+this.ws = null;
 
-ws.on('open', function() {
-	info('Connected to ' + 'RIPPLED'.bold.blue + ' successfully...');
+// retreive meta data
+var meta = readMeta(function (meta) {
+	self = this;
 
-	var msg = {
-		"command"	: "subscribe",
-		"accounts"	: [ RIPPLE_ADDRESS ]
-	}
+	// retreive current ledger index
+	var curLedgerIdx = "n/a";
 
-	// send subscription message
-	ws.send(JSON.stringify(msg));
+	debug('Current Ledger Index: '.bold.cyan + curLedgerIdx);
+	debug('Last Ledger Index:    '.bold.cyan + meta.ledger);
+	debug('Last Tx Hash:         '.bold.cyan + meta.hash);
+	debug('Last Timesatmp:       '.bold.cyan + meta.timestamp);
+	console.log();		// spacer
+
+	// TODO: add support for server resume
+	//		 we will need to retrieve the most recent
+	// 		 ledger index and compare to meta data
+	//
+	//		 process all PENDING transaction BEFORE
+	// 		 we send subscribe request to rippled
+
+	// connect to RIPPLED socket server
+	this.ws = new WebSocket(RIPPLE_SERVER);
+
+	this.ws.on('open', function() {
+		info('Connected to ' + 'RIPPLED'.bold.blue + ' successfully...');
+
+		var msg = {
+			"command"	: "subscribe",
+			"accounts"	: [ RIPPLE_ADDRESS ]
+		}
+
+var index = 0;
+var msg = {
+	"command"	: 'tx_history',
+	"params"	: [{'start' : index}]
+}
+
+		// send subscription message
+		self.ws.send(JSON.stringify(msg));
+	});
+
+	this.ws.on('message', function(message) {
+debug(message);
+return;
+		// parse the server's response
+		var data = parseResponse(message);
+
+		if (data.validated && data.account != RIPPLE_ADDRESS) {
+			logSpacer();	// log spacer
+
+			debug('Player Account:   '.bold + data.account);
+			debug('Ledger Index:     '.bold + data.ledger);
+			debug('Transaction Hash: '.bold + data.hash);
+			debug('Bet Amount:       '.bold + parseInt(data.amount / RIPPLE_SINGLE));
+			debug('Our Bank Roll:    '.bold + parseInt(data.bankroll / RIPPLE_SINGLE) + ' XRP');
+
+			// do we have a destination tag
+			// TODO: detect tag and perform specific actions
+			// 		 e.g. 777 = refill transaction (DO NOT RUN GAMES)
+			if (data.tag)
+				debug('Destination Tag:  '.bold + data.tag);
+
+			// run the Coin Flip game
+			runCoinFlip(data);
+		} else if (data.result) {
+			logSpacer();	// log spacer
+
+			debug(data.message);
+		} else {
+//			logSpacer();	// log spacer
+
+			// TODO: we should provide confirmation that the payment
+			// 		 was sent, received and processed successfully
+//			debug(message);
+		}
+	});
 });
 
-ws.on('message', function(message) {
-	// parse the server's response
-	var data = parseResponse(message);
-
-	if (data.validated && data.account != RIPPLE_ADDRESS) {
-		logSpacer();	// log spacer
-
-		debug('Player Account: ' 	+ data.account);
-		debug('Tx Hash: ' 			+ data.hash);
-		debug('Bet Amount: ' 		+ parseInt(data.amount / RIPPLE_SINGLE));
-		debug('Our Bank Roll: ' 	+ parseInt(data.bankroll / RIPPLE_SINGLE) + ' XRP');
-
-		// run the Hi-Lo game
-		runHiLo(data);
-	} else if (data.result) {
-		logSpacer();	// log spacer
-
-		debug(data.message);
-	} else {
-		// TODO: we should provide confirmation that the payment
-		// 		 was sent, received and processed successfully
-//		logSpacer();	// log spacer
-//		debug(message);
-	}
-});
 
 /**
  * Simple 50/50 coin flip game.
@@ -133,18 +175,18 @@ ws.on('message', function(message) {
  * If user wins, the payout is calclated based 
  * on the venue's Win Percentage.
  */
-function runHiLo(data) {
-	info('STARTING Hi-Lo Game');
+function runCoinFlip(data) {
+	info('Starting Coin Flip Game'.bold.blue);
 
 	var gameType 	= 16;		// 16-bit gameplay
 	var payout 		= 0;
 
 	// set to 20% of total bankroll
 	var betLimit = parseInt(data.bankroll / 5);
-	debug('Bet Limit: ' + betLimit + ' (' + parseInt(betLimit / RIPPLE_SINGLE) + ' XRP)');
+	debug('Bet Limit: '.bold + betLimit + ' (~' + parseInt(betLimit / RIPPLE_SINGLE) + ' XRP)');
 
 	// calculate the fairplay point
-	var point = calcFairplay(data, gameType);
+	var point = fairplayPoint(data, gameType);
 
 	// calculate win/loss payout
 	if (data.amount > betLimit) {
@@ -160,17 +202,17 @@ function runHiLo(data) {
 		payout = parseInt(data.amount * VENUE_WIN_PAYOUT) + data.amount;
 	} else {
 		// this is a loss -- send back dust
-		info('LOSER.'.bold.yellow)
+		info('LOSER'.bold.yellow)
 		payout = 100;
 	}
 
-	info('Payout: ' + payout);
+	info('Payout: '.bold.blue + payout + ' (~' + parseInt(payout / RIPPLE_SINGLE) + ' XRP)');
 
 	// construct the payment message
 	// TODO: for security reasons, its best to sign the transaction
 	// 		 BEFORE sending it off to the ripple daemon
 	//		 in order to protect the address' secret key
-	msg = {
+	var msg = {
 		'command' : 'submit',
 		'tx_json' : {
 			'TransactionType' : 'Payment',
@@ -187,21 +229,26 @@ function runHiLo(data) {
 
 	// send the payment transaction
 	debug(JSON.stringify(msg));
-	ws.send(JSON.stringify(msg));
+	this.ws.send(JSON.stringify(msg));
+
+	// update meta data
+	writeMeta(data);
 }
 
 /**
- * Calculate the hash for proper fairplay rules
+ * Calculate the point value based on proper fairplay rules
  */
-function calcFairplay(data, gameType) {
-	info('Calculating Fairplay for ' + gameType + '-bit Game...');
+function fairplayPoint(data, gameType) {
+	info('Calculating Fairplay for '.bold.blue + 
+		gameType.toString().bold.yellow + '-bit'.bold.yellow + 
+		' Game...'.bold.blue);
 
 	// init hmac 256
 	var hmac = crypto.createHmac('sha256', VENUES_SECRET);
 
 	// update with txid hash
 	var hmacHash = hmac.update(data.hash).digest('hex');
-	debug('hmacHash: ' + hmacHash.toString().toUpperCase());
+	debug('hmacHash:  '.bold + hmacHash.toString().toUpperCase());
 
 	// calculate the number of bytes in the game
 	// gameType / 4 = numChars
@@ -210,11 +257,11 @@ function calcFairplay(data, gameType) {
 
 	// slice the number of bytes needed
 	var hashBytes = hmacHash.slice(0, numChars);
-	debug('hashBytes: ' + hashBytes.toString().toUpperCase());
+	debug('hashBytes: '.bold + hashBytes.toString().toUpperCase());
 
 	// calculate the int value from hex slice
 	var hashInt	= parseInt(hashBytes, 16);
-	debug('hashInt: ' + hashInt);
+	debug('hashInt:   '.bold + hashInt);
 
 	return hashInt;
 }
@@ -231,12 +278,17 @@ function parseResponse(message, subAddr) {
 	if (json.type == "transaction") {
 		data.status 	= json.status;
 		data.validated 	= json.validated;
+		data.ledger 	= json.ledger_index
 
 		data.account 	= json.transaction.Account;
 		data.amount 	= parseInt(json.transaction.Amount);
 		data.fee 		= parseInt(json.transaction.Fee);
 		data.date 		= json.transaction.date;
 		data.hash 		= json.transaction.hash;
+
+		// check for destination tag
+		if (json.transaction.DestinationTag)
+			data.tag 	= json.transaction.DestinationTag;
 
 		// we need the server's bankroll
 		data.bankroll 	= getBankroll(json);
@@ -271,6 +323,61 @@ function getBankroll(json) {
 	});
 
 	return bankroll;
+}
+
+// read meta data from file
+function readMeta(callback) {
+	// initialize server's persistent data
+	fs.readFile('meta.json', 'utf8', function (err, data) {
+		if (err) {
+			error(JSON.stringify(err));
+
+			if (err.code == "ENOENT") {
+				// write default data to meta storage
+				return writeMeta();
+			}
+
+			return;
+		}
+
+		// return data
+		callback(JSON.parse(data));
+	});
+}
+
+// write meta data to file
+function writeMeta(data) {
+	if (typeof data === 'undefined') {
+		var ledger 	= 0;
+		var hash 	= "";
+	} else {
+		var ledger 	= data.ledger;
+		var hash 	= data.hash;
+	}
+
+	// linux timestamp
+	var timesatmp = Math.round((new Date()).getTime() / 1000);
+
+	// default meta data
+	var meta = {
+		'ledger'	: ledger,
+		'hash'		: hash,
+		'timestamp'	: timesatmp
+	}
+
+	// write the new data
+	fs.writeFile('meta.json', JSON.stringify(meta), function (err) {
+		if (err) {
+			error(err.message);
+
+			return;
+		}
+
+		info('meta.json'.bold.blue + ' saved successfully...');
+	});
+
+	// return meta data
+	return meta;
 }
 
 // setup logger helpers
